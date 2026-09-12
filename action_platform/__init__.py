fb595803de98717158f5cfc429a83832ba544598
Fernando Celmer <email@fernandocelmer.com>
2026-09-12 04:28:06 -0300

    chore(platform): align pyproject and __version__ with LAST_VERSION 0.1.2

--- action_platform/__init__.py
+++ action_platform/__init__.py
@@ -1,6 +1,6 @@
 """Action Platform __init__ module."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __description__ = "🛠️ Action Platform padroniza init, release e deploy."
 
 from .core.config import Config
